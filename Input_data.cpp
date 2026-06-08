e462004bed68b4623bc224b5c3b6079ae7bc36a8
2503010055-Fahri <2503010055@unper.ac.id>
2026-06-08 21:38:03 +0700

    Menambahkan input data ortu

--- Input_data.cpp
+++ Input_data.cpp
@@ -2,17 +2,8 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
     string nama, alamat, pekerjaan, alamatPekerjaan, asalSekolah;
-=======
-<<<<<<< HEAD
-    string nama, alamat;
-    int umur;
-=======
-    string nama, alamat, pekerjaan, alamatPekerjaan;
->>>>>>> 23b2288ae2444b101015f7838d83ec36685e920a
     int umur, gaji;
->>>>>>> b5d9c97f649ab9e4c969e8b7d73255d5fd84d85a
 
     string namaOrtuAyah, namaOrtuIbu, alamatAyah, alamatIbu, TamatLulusanOrtu;
     int gajiOrtuAyah, gajiOrtuIbu, pilLulusanOrtu;
@@ -21,13 +12,6 @@ int main(){
     getline(cin, nama);
 
     cout << "Masukan alamat : ";
-<<<<<<< HEAD
-    cin.ignore();
-    getline(cin, alamat);
-
-    cout << "Usia : ";
-    cin >> umur;
-=======
     getline(cin, alamat);
 
     cout << "Asal Sekolah : ";
@@ -37,26 +21,24 @@ int main(){
     getline(cin, pekerjaan);
 
     cout << "Alamat Pekerjaan : ";
-    cin.ignore();
     getline(cin, alamatPekerjaan);
 
     cout << "Usia : ";
     cin >> umur;
 
-    cout << "Gaji/Moon : ";
+    cout << "Gaji/Bulan : ";
     cin >> gaji;
 
     cout << endl;
 
     cout << "======= DATA DIRI =======\n";
-    cout << "Nama            : " << nama << endl;
-    cout << "Alamat          : " << alamat << endl;
-    cout << "Asal Sekolah    : " << asalSekolah << endl;
-    cout << "Pekerjaan       : " << pekerjaan << endl;
-    cout << "Almat Pekerjaan : " << alamatPekerjaan << endl;
-    cout << "Usia            : " << umur << endl;
-    cout << "Gaji/Moon       : " << gaji << endl;
-<<<<<<< HEAD
+    cout << "Nama               : " << nama << endl;
+    cout << "Alamat             : " << alamat << endl;
+    cout << "Asal Sekolah       : " << asalSekolah << endl;
+    cout << "Pekerjaan          : " << pekerjaan << endl;
+    cout << "Alamat Pekerjaan   : " << alamatPekerjaan << endl;
+    cout << "Usia               : " << umur << endl;
+    cout << "Gaji/Bulan         : " << gaji << endl;
 
     cout << endl;
     cin.ignore();
@@ -67,7 +49,7 @@ int main(){
     cout << "Nama Orang Tua Ibu : ";
     getline(cin, namaOrtuIbu);
 
-    cout << "Alamat ayah : ";
+    cout << "Alamat Ayah : ";
     getline(cin, alamatAyah);
 
     cout << "Alamat Ibu : ";
@@ -82,11 +64,12 @@ int main(){
     cin.ignore();
 
     // Pendidikan terakhir orang tua
-    cout << "\n==== PENDIDKAN TERAKHIR ORANG TUA ====\n";
+    cout << "\n==== PENDIDIKAN TERAKHIR ORANG TUA ====\n";
     cout << "1. SD\n";
     cout << "2. SMP\n";
     cout << "3. SMA\n";
     cout << "4. S1\n";
+    cout << "Masukkan pilihan : ";
     cin >> pilLulusanOrtu;
 
     switch(pilLulusanOrtu){
@@ -106,15 +89,14 @@ int main(){
             TamatLulusanOrtu = "Tidak dapat diakses";
     }
 
-    cout << "==== DATA ORANG TUA ====\n";
+    cout << "\n==== DATA ORANG TUA ====\n";
     cout << "Nama Ayah          : " << namaOrtuAyah << endl;
     cout << "Nama Ibu           : " << namaOrtuIbu << endl;
-    cout << "Alama Ayah         : " << alamatAyah << endl;
+    cout << "Alamat Ayah        : " << alamatAyah << endl;
     cout << "Alamat Ibu         : " << alamatIbu << endl;
     cout << "Gaji Ayah / Bulan  : " << gajiOrtuAyah << endl;
     cout << "Gaji Ibu / Bulan   : " << gajiOrtuIbu << endl;
     cout << "Lulusan Pendidikan : " << TamatLulusanOrtu << endl;
-=======
->>>>>>> b5d9c97f649ab9e4c969e8b7d73255d5fd84d85a
->>>>>>> 23b2288ae2444b101015f7838d83ec36685e920a
+
+    return 0;
 }
